85bf7963c62e0448400f62d4a1d3c619ac5ca8f2
Yikuan Sun <38194363+yikuansun@users.noreply.github.com>
2023-04-06 11:29:10 -0400

    Edit landing page text

--- src/App.svelte
+++ src/App.svelte
@@ -395,11 +395,11 @@ Reference Image
         <div class={"centered"} style={"text-align: center;"}>
             <img alt="PROGEN FLARES 2" src="./textLogo.png" width="321" />
             <br /> <br />
-            <span style={"width: 145px; text-align: left; display: inline-block;"}>Doc Width</span> <input type="number" bind:value={flareSettings.dimensions.width} style={"width: 80px;"} />
+            <span style={"width: 145px; text-align: left; display: inline-block;"}>Image Width</span> <input type="number" bind:value={flareSettings.dimensions.width} style={"width: 80px;"} />
             <br />
-            <span style={"width: 145px; text-align: left; display: inline-block;"}>Doc Height</span> <input type="number" bind:value={flareSettings.dimensions.height} style={"width: 80px;"} />
+            <span style={"width: 145px; text-align: left; display: inline-block;"}>Image Height</span> <input type="number" bind:value={flareSettings.dimensions.height} style={"width: 80px;"} />
             <br /> <br />
-            <button on:click={onStart}>go</button>
+            <button on:click={onStart}>Create</button>
             <br /> <br />
             <span style={"font-size: 10px;"}>&copy; 2023 Lunal Graphics<br />Developed by Yikuan Sun</span>
         </div>
