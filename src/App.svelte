a520d09820a2fa38d425e9b5643bc93a1ad3e4b2
Yikuan Sun <38194363+yikuansun@users.noreply.github.com>
2026-06-26 12:03:53 +0800

    Hide upload image button in Photoshop/photopea

--- src/App.svelte
+++ src/App.svelte
@@ -397,14 +397,16 @@
     </select>
     <span style="white-space: pre; color: grey;">{"  |  "}</span>
     <input type="checkbox" bind:this={rIcheckbox} on:change={handleRIcheckbox} style="margin-bottom: 0;" />
-    {#if layout === "horizontal"}Reference Image{:else}Image{/if}
-    <button on:click={handleRIbutton} title="Import reference image" style="padding: 4px 8px; line-height: 1;">
-      <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-        <path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4"/>
-        <polyline points="17 8 12 3 7 8"/>
-        <line x1="12" y1="3" x2="12" y2="15"/>
-      </svg>
-    </button>
+    {#if layout === "horizontal"}Reference Image{:else}Reference{/if}
+    {#if !isPhotopeaPlugin && !isPhotoshopPlugin}
+      <button on:click={handleRIbutton} title="Import reference image" style="padding: 4px 8px; line-height: 1;">
+        <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+          <path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4"/>
+          <polyline points="17 8 12 3 7 8"/>
+          <line x1="12" y1="3" x2="12" y2="15"/>
+        </svg>
+      </button>
+    {/if}
   </div>
 </div>
 
